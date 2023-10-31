e5eedc131ff0fcd61a07e273dfab500ecc1530c2
cognaiger <21020369@vnu.edu.vn>
2023-11-01 00:30:39 +0700

    add dashboard

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/authContext';
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
+      <ChakraProvider>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- client/src/screens/Boss/BDashboard/BDashboard.jsx
+++ client/src/screens/Boss/BDashboard/BDashboard.jsx
@@ -1,8 +1,63 @@
 import React from 'react';
+import "./BDashBoard.scss";
+import { Button, Card, CardBody, CardHeader, Heading, Menu, MenuButton, MenuItem, MenuList, SimpleGrid, Text } from '@chakra-ui/react';
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import LocalPostOfficeOutlinedIcon from '@material-ui/icons/LocalPostOfficeOutlined';
+import LocalShippingOutlinedIcon from '@material-ui/icons/LocalShippingOutlined';
+import CallReceivedOutlinedIcon from '@material-ui/icons/CallReceivedOutlined';
 
 const BDashboard = () => {
   return (
-    <div>BDashboard</div>
+    <div className='bdashboard'>
+        <div className='top'>
+            <div className='title'>Dashboard</div>
+            <div className='select'>
+                <Menu>
+                    <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                        Choose point type
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem>Exchange Point</MenuItem>
+                        <MenuItem>Collection Point</MenuItem>
+                    </MenuList>
+                </Menu>
+            </div>
+        </div>
+
+        <div className='content'>
+            <div className='number'>
+                <SimpleGrid spacing={4} templateColumns='1fr 1fr 1fr' columnGap={40}>
+                    <Card width={400} height={250} borderRadius={8} backgroundColor='#9CFF84'>
+                        <CardHeader display='flex' justifyContent='space-between'>
+                            <Heading size='md'>Sent Packages</Heading>
+                            <LocalPostOfficeOutlinedIcon />
+                        </CardHeader>
+                        <CardBody>
+                            <Text fontSize={50} fontWeight={700}>500</Text>
+                        </CardBody>
+                    </Card>
+                    <Card width={400} height={250} borderRadius={8} backgroundColor='#FF738C'>
+                        <CardHeader display='flex' justifyContent='space-between'>
+                            <Heading size='md'>Pending Packages</Heading>
+                            <LocalShippingOutlinedIcon />
+                        </CardHeader>
+                        <CardBody>
+                            <Text fontSize={50} fontWeight={700}>500</Text>
+                        </CardBody>
+                    </Card>
+                    <Card width={400} height={250} borderRadius={8} backgroundColor='#9EC5FF'>
+                        <CardHeader display='flex' justifyContent='space-between'>
+                            <Heading size='md'>Received Packages</Heading>
+                            <CallReceivedOutlinedIcon />
+                        </CardHeader>
+                        <CardBody>
+                            <Text fontSize={50} fontWeight={700}>500</Text>
+                        </CardBody>
+                    </Card>
+                </SimpleGrid>
+            </div>
+        </div>
+    </div>
   )
 }
 
