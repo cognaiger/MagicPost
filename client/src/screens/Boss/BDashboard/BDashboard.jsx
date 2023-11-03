e498354f084c1e24debb07e5422fc410d9eb2c78
cognaiger <21020369@vnu.edu.vn>
2023-11-03 16:59:49 +0700

    modify bdashboard

--- client/src/screens/Boss/BDashboard/BDashboard.jsx
+++ client/src/screens/Boss/BDashboard/BDashboard.jsx
@@ -7,58 +7,60 @@ import LocalShippingOutlinedIcon from '@material-ui/icons/LocalShippingOutlined'
 import CallReceivedOutlinedIcon from '@material-ui/icons/CallReceivedOutlined';
 
 const BDashboard = () => {
-  return (
-    <div className='bdashboard'>
-        <div className='top'>
-            <div className='title'>Dashboard</div>
-            <div className='select'>
-                <Menu>
-                    <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                        Choose point type
-                    </MenuButton>
-                    <MenuList>
-                        <MenuItem>Exchange Point</MenuItem>
-                        <MenuItem>Collection Point</MenuItem>
-                    </MenuList>
-                </Menu>
+    return (
+        <div className='bdashboard'>
+            <div className='top'>
+                <div className='title'>Dashboard</div>
+                <div className='select'>
+                    <Menu>
+                        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                            Choose branch
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem>All</MenuItem>
+                            <MenuItem>Branch 1</MenuItem>
+                            <MenuItem>Branch 2</MenuItem>
+                        </MenuList>
+                    </Menu>
+                </div>
             </div>
-        </div>
 
-        <div className='content'>
-            <div className='number'>
-                <SimpleGrid spacing={4} templateColumns='1fr 1fr 1fr' columnGap={40}>
-                    <Card width={300} height={250} borderRadius={8} backgroundColor='#9CFF84'>
-                        <CardHeader display='flex' justifyContent='space-between'>
-                            <Heading size='md'>Sent Packages</Heading>
-                            <LocalPostOfficeOutlinedIcon />
-                        </CardHeader>
-                        <CardBody>
-                            <Text fontSize={50} fontWeight={700}>500</Text>
-                        </CardBody>
-                    </Card>
-                    <Card width={300} height={250} borderRadius={8} backgroundColor='#FF738C'>
-                        <CardHeader display='flex' justifyContent='space-between'>
-                            <Heading size='md'>Pending Packages</Heading>
-                            <LocalShippingOutlinedIcon />
-                        </CardHeader>
-                        <CardBody>
-                            <Text fontSize={50} fontWeight={700}>500</Text>
-                        </CardBody>
-                    </Card>
-                    <Card width={300} height={250} borderRadius={8} backgroundColor='#9EC5FF'>
-                        <CardHeader display='flex' justifyContent='space-between'>
-                            <Heading size='md'>Received Packages</Heading>
-                            <CallReceivedOutlinedIcon />
-                        </CardHeader>
-                        <CardBody>
-                            <Text fontSize={50} fontWeight={700}>500</Text>
-                        </CardBody>
-                    </Card>
-                </SimpleGrid>
+            <div className='content'>
+                <div className='branch'>Branch 1 (Collection Point)</div>
+                <div className='number'>
+                    <SimpleGrid spacing={4} templateColumns='1fr 1fr 1fr' columnGap={40}>
+                        <Card width={350} height={200} borderRadius={8} backgroundColor='#9CFF84'>
+                            <CardHeader display='flex' justifyContent='space-between'>
+                                <Heading size='md'>Sent Packages</Heading>
+                                <LocalPostOfficeOutlinedIcon />
+                            </CardHeader>
+                            <CardBody>
+                                <Text fontSize={50} fontWeight={700}>2,500</Text>
+                            </CardBody>
+                        </Card>
+                        <Card width={350} height={200} borderRadius={8} backgroundColor='#FF738C'>
+                            <CardHeader display='flex' justifyContent='space-between'>
+                                <Heading size='md'>Pending Packages</Heading>
+                                <LocalShippingOutlinedIcon />
+                            </CardHeader>
+                            <CardBody>
+                                <Text fontSize={50} fontWeight={700}>2,500</Text>
+                            </CardBody>
+                        </Card>
+                        <Card width={350} height={200} borderRadius={8} backgroundColor='#9EC5FF'>
+                            <CardHeader display='flex' justifyContent='space-between'>
+                                <Heading size='md'>Received Packages</Heading>
+                                <CallReceivedOutlinedIcon />
+                            </CardHeader>
+                            <CardBody>
+                                <Text fontSize={50} fontWeight={700}>3,500</Text>
+                            </CardBody>
+                        </Card>
+                    </SimpleGrid>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default BDashboard;
