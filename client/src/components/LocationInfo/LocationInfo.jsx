5bbe02e7f3625d127454a15c26276b91fd24ad2c
cognaiger <21020369@vnu.edu.vn>
2023-11-04 14:18:27 +0700

    add location management screen

---
+++ client/src/components/LocationInfo/LocationInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import './LocationInfo.scss';
+
+const LocationInfo = ({ name, location, head, type }) => {
+    return (
+        <div className='detail'>
+            <div className='name'>{name}</div>
+            <div className='info'>
+                <div className='prop'>
+                    <div className='key'>Location</div>
+                    <div className='value'>{location}</div>
+                </div>
+                <div className='prop'>
+                    <div className='key'>Head</div>
+                    <div className='value'>{head}</div>
+                </div>
+                <div className='prop'>
+                    <div className='key'>Type</div>
+                    <div className='value'>{type}</div>
+                </div>
+            </div>
+            <MoreVertIcon />
+        </div>
+    )
+}
+
+export default LocationInfo;
--- client/src/screens/Boss/BLocation/BLocation.jsx
+++ client/src/screens/Boss/BLocation/BLocation.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import './BLocation.scss';
+import { Menu, MenuButton, MenuItem, MenuList, Button, Divider } from '@chakra-ui/react';
+import { AddIcon, ChevronDownIcon } from '@chakra-ui/icons';
+import LocationInfo from '../../../components/LocationInfo/LocationInfo';
 
 const BLocation = () => {
   return (
-    <div>BLocation</div>
+    <div className='blocation'>
+      <div className='top'>
+        <div className='title'>Location Management</div>
+        <Divider />
+      </div>
+
+      <div className='option'>
+        <Button colorScheme='purple' leftIcon={<AddIcon />}>
+          Add location
+        </Button>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            Choose type
+          </MenuButton>
+          <MenuList>
+            <MenuItem>All</MenuItem>
+            <MenuItem>Exchange Point</MenuItem>
+            <MenuItem>Collection Point</MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
+
+      <div className='info'>
+        <LocationInfo name='Transaction Point 2' location='212 Wall Street, Pa, New York' head='Jame Miller' type='Transaction Point' />
+        <LocationInfo name='Transaction Point 2' location='212 Wall Street, Pa, New York' head='Jame Miller' type='Transaction Point' />
+        <LocationInfo name='Transaction Point 2' location='212 Wall Street, Pa, New York' head='Jame Miller' type='Transaction Point' />
+      </div>
+    </div>
   )
 }
 
