6ddaf4a697a4da93d1f3a6f1b4e626a359b15740
suracI-invert <koolsha123@gmail.com>
2023-12-10 02:34:30 +0700

    add tracking page on landing, styles still need touching

--- client/src/screens/LandingPage/LandingPage.jsx
+++ client/src/screens/LandingPage/LandingPage.jsx
@@ -1,11 +1,36 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import logo from "../../img/logo.png";
 import map from "../../img/mapbig.png";
 import "./LandingPage.scss";
 import Button from "../../components/Button/Button";
+import TrackingBar from "../../components/TrackingBar/TrackingBar"
 
 const LandingPage = () => {
     const navigate = useNavigate();
+    const [content, setContent] = useState('services');
+
+    // Temporary
+    const Services = () => {
+      return (
+        <>
+          <div className="left">
+            <div className="ti1">Your lighting-fast delivery partner</div>
+            <div className="ti2">MagicPost is a logistic company providing courier, package delivery and express mail service. It helps everyone to send
+              documents, packages, parcels and letter to desired destination
+            </div>
+            <div className="line"></div>
+            <div className="statistic">
+              <div>24/7 Expedition</div>
+              <div>5M+ Packages Delivered</div>
+            </div>
+          </div>
+          <div className="right">
+            <img src={map} alt="map" />
+        </div>
+        </>
+      )
+    }
 
   return (
     <div className="page">
@@ -16,9 +41,9 @@ const LandingPage = () => {
         </div>
 
         <div className="menu">
-          <div>Services</div>
-          <div>Tracking</div>
-          <div>Locations</div>
+          <button onClick={() => setContent('services')}>Services</button>
+          <button onClick={() => setContent('tracking')}>Tracking</button>
+          <button onClick={() => setContent('locations')}>Locations</button>
         </div>
 
         <div className="button">
@@ -28,21 +53,8 @@ const LandingPage = () => {
       </div>
 
       <div className="content">
-        <div className="left">
-          <div className="ti1">Your lighting-fast delivery partner</div>
-          <div className="ti2">MagicPost is a logistic company providing courier, package delivery and express mail service. It helps everyone to send
-            documents, packages, parcels and letter to desired destination
-          </div>
-          <div className="line"></div>
-          <div className="statistic">
-            <div>24/7 Expedition</div>
-            <div>5M+ Packages Delivered</div>
-          </div>
-        </div>
-
-        <div className="right">
-          <img src={map} alt="map" />
-        </div>
+          {content == 'services' && <Services/>}
+          {content == 'tracking' && <TrackingBar/>}
       </div>
     </div>
   );
