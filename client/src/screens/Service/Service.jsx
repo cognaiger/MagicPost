3b46d8f5193721fed148cc5ab32e4e2fa8328d4a
phong812 <phamgiaphong8122003@gmail.com>
2023-12-25 23:25:31 +0700

    update service
    list service content

--- client/src/screens/Service/Service.jsx
+++ client/src/screens/Service/Service.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import "./Service.scss";
 
+
 function Service() {
   return (
-    <div>Service</div>
+    <div className="service">
+      <div className="card">
+        <div className="top">
+          <h1>DỊCH VỤ TRONG NƯỚC</h1>
+            <ul className="submenu">
+              <li>Chuyển phát hỏa tốc</li>
+              <li>Chuyển phát nhanh</li>
+              <li>Chuyển phát tiết kiệm</li>
+            </ul>
+        </div>
+        <div className="mid">
+          <h2>Dịch vụ quốc tế</h2>
+        </div>
+        <div className="bot">
+          <h2>Dịch vụ Logistics</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
